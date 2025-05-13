31d7f2e4ec541349c84ae30e4cc78a87c3d90a44
Cecep Rokani <cecepfahriazal1997@gmail.com>
2025-05-13 11:24:33 +0700

    fix: rebuild

--- src/views/task/List.vue
+++ src/views/task/List.vue
@@ -7,8 +7,8 @@
                         <!-- title of page -->
                         <div class="row">
                             <div class="col-12 mt-3">
-                                <div class="page-title-box d-sm-flex align-items-start justify-content-between">
-                                    <h4 class="mb-sm-0">Data Transaksi</h4>
+                                <div class="page-title-box d-flex align-items-start justify-content-between">
+                                    <h4 class="mb-sm-0">Daftar Tugas</h4>
                                     <router-link to="/task/form" class="btn btn-primary button-rounded ms-2" type="button"><i class="mdi mdi-plus me-1"></i>Tambah Tugas Baru</router-link>
                                 </div>
                             </div>
